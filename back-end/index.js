51c0a71ac905bfdff5ab21fde02940bc3d676dd4
ЗОЛЗАЯА Бат-Эрдэнэ <you@example.com>
2024-10-31 14:05:56 +0800

    get, post methods

--- back-end/index.js
+++ back-end/index.js
@@ -1,8 +1,51 @@
-import express from "express";
+import express, { request, response } from "express";
+import bodyParser from "body-parser";
 
 const app = express();
+app.use(bodyParser.json());
 const port = 4000;
 
+const users = [
+  { name: "Zoloo", age: 85 },
+  { name: "Dash", age: 80 },
+  { name: "Zulaa", age: 75 },
+];
+
+app.get("/users", (request, response) => {
+  response.send(users);
+});
+
+app.post("/users", (request, response) => {
+  const { name, age } = request.body;
+  response.send(request.body);
+
+  if (!name || typeof age !== "number") {
+    return response.send({ error: "name and age are required" });
+  }
+
+  const newUser = { name, age };
+  users.push(newUser);
+  response.send(newUser);
+});
+app.put("/users", (request, response) => {
+  response.send("Hello Put huselt");
+});
+
+app.delete("/users", (req, res) => {
+  const { name } = req.body; // Expecting name in the request body
+
+  // Find the index of the user to delete
+  const index = users.findIndex((user) => user.name === name);
+
+  if (index === -1) {
+    return res.status(404).send({ message: "User not found" });
+  }
+
+  // Remove the user from the array
+  const deletedUser = users.splice(index, 1);
+  res.send({ message: "User deleted successfully", user: deletedUser });
+});
+
 app.listen(port, () => {
-  console.log("server is working");
+  console.log(`Server is running on http://localhost: ${port}`);
 });
